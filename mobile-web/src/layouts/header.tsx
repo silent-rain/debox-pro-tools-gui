c5dabc0782b6aeae24bd49fac3e178d9ccbcec43
silent-rain <2367221387@qq.com>
2025-10-24 10:30:11 +0800

    首页优化

--- mobile-web/src/layouts/header.tsx
+++ mobile-web/src/layouts/header.tsx
@@ -10,9 +10,15 @@ interface HeaderProps {
 const Header = ({ title }: HeaderProps) => {
   const navigate = useNavigate();
 
+  // 首页不显示返回按钮
+  if (title === '首页') {
+    return null;
+  }
+
   return (
     <NavBar
       className={styles.header}
+      backIcon={false}
       onBack={() => {
         navigate(-1);
       }}
