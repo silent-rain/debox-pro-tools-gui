d46c76e5bac426c4d9926e3dd7cb07af11f2bbe9
silent-rain <2367221387@qq.com>
2025-10-30 18:14:00 +0800

    调试tailwindcss样式问题

--- mobile-web/tailwind.config.mjs
+++ mobile-web/tailwind.config.mjs
@@ -4,6 +4,8 @@ export default {
     './index.html', // 如果有使用到 HTML 文件
     './src/**/*.{html,js,ts,jsx,tsx,css}', // 扫描 src 目录下所有 js,ts,jsx,tsx 文件
   ],
+  prefix: 'tw-', // 为所有 Tailwind 类添加前缀，避免与其他框架的类冲突
+  preflight: false, // 禁用全局样式重置
   theme: {
     extend: {},
   },
