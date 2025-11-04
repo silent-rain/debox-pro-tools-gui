bf944888807ea560af4ee238a8ec019fa28d14c2
silent-rain <2367221387@qq.com>
2025-11-04 15:21:22 +0800

    404页面优化

--- mobile-web/src/pages/error404/index.tsx
+++ mobile-web/src/pages/error404/index.tsx
@@ -1,13 +1,21 @@
+import { Button, ErrorBlock } from 'antd-mobile';
+import { useNavigate } from 'react-router';
+import { ROUTES } from '@/constants/routes';
 import styles from './index.module.css';
 
 export default function Error404() {
+  const navigate = useNavigate();
+
+  const click = () => {
+    navigate(ROUTES.HOME);
+  };
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>404</h1>
-      <p className={styles.message}>Oops! The page you're looking for doesn't exist.</p>
-      <button className={styles.button} onClick={() => (window.location.href = '/')}>
+      <ErrorBlock status='default' />
+      <Button className={styles.button} color='primary' fill='solid' onClick={click}>
         Return to Home
-      </button>
+      </Button>
     </div>
   );
 }
