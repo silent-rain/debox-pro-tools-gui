c028268b017665098e1c3f4b020b62a2f5e7dca8
silent-rain <2367221387@qq.com>
2025-11-04 10:18:38 +0800

    临时修复tailwindcss样式冲突问题

--- mobile-web/tailwind.config.mjs
+++ mobile-web/tailwind.config.mjs
@@ -2,14 +2,15 @@
 export default {
   content: [
     './index.html', // 如果有使用到 HTML 文件
-    './src/**/*.{html,js,ts,jsx,tsx,css}', // 扫描 src 目录下所有 js,ts,jsx,tsx 文件
+    './src/**/*.{js,ts,jsx,tsx}', // 扫描 src 目录下所有 js,ts,jsx,tsx 文件
   ],
-  prefix: 'tw-', // 为所有 Tailwind 类添加前缀，避免与其他框架的类冲突
-  theme: {
-    extend: {},
-  },
-  plugins: [],
   corePlugins: {
     preflight: false, // 禁用全局样式重置
   },
+  // prefix: 'tw-', // 为所有 Tailwind 类添加前缀，避免与其他框架的类冲突
+  theme: {
+    extend: {},
+  },
+  purge: [],
+  plugins: [],
 };
