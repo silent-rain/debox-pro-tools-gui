dd506ab738eafd8ab6ff6b75b7c36020f910bc93
silent-rain <2367221387@qq.com>
2025-12-05 00:16:55 +0800

    私聊页面优化

--- mobile-web/src/pages/message/index.tsx
+++ mobile-web/src/pages/message/index.tsx
@@ -34,39 +34,66 @@ const SendMessageManagement = () => {
   };
 
   return (
-    <div className='send-message-management'>
-      {/* 选择账号 */}
-      <Dropdown defaultActiveKey='account'>
-        <Dropdown.Item key='account' title='选择账号'>
-          <AccountList
-            defaultSelected
-            onChange={(accountIds) => {
-              setAccountId(accountIds[0]);
-            }}
-          />
-        </Dropdown.Item>
-      </Dropdown>
-
-      <div className={styles.operationButton}>
-        <Button color='primary' size='small' fill='solid' onClick={handleRefreshFriends}>
+    <div className={styles.sendMessageManagement}>
+      {/* 顶部操作栏：账号选择和刷新按钮 */}
+      <div className={styles.topBar}>
+        <div className={styles.accountSelector}>
+          <Dropdown defaultActiveKey='account'>
+            <Dropdown.Item key='account' title='选择账号'>
+              <AccountList
+                defaultSelected
+                onChange={(accountIds) => {
+                  setAccountId(accountIds[0]);
+                }}
+              />
+            </Dropdown.Item>
+          </Dropdown>
+        </div>
+        <Button
+          color='primary'
+          size='small'
+          fill='solid'
+          onClick={handleRefreshFriends}
+          className={styles.refreshButton}
+        >
           刷新
         </Button>
       </div>
 
-      <AccountFriendList
-        accountId={accountId}
-        multiple
-        friendsUpdateState={friendsUpdateState}
-        onChange={(toUserIds: string[]) => {
-          setToUserIds(toUserIds);
-        }}
-      />
+      {/* 消息输入区域 - 移到顶部下方 */}
+      <div className={styles.messageSection}>
+        <div className={styles.inputContainer}>
+          <Input
+            placeholder='请输入消息内容'
+            clearable
+            value={content}
+            onChange={(value) => setContent(value)}
+            className={styles.messageInput}
+          />
+          <Button
+            color='primary'
+            size='small'
+            fill='solid'
+            onClick={handleSendMessage}
+            disabled={!content.trim() || toUserIds.length === 0}
+            className={styles.sendButton}
+          >
+            发送
+          </Button>
+        </div>
+        {toUserIds.length > 0 && <div className={styles.selectedInfo}>已选择 {toUserIds.length} 个好友</div>}
+      </div>
 
-      <div className='send-message'>
-        <Input placeholder='请输入消息内容' clearable value={content} onChange={(value) => setContent(value)} />
-        <Button color='primary' size='small' fill='solid' onClick={handleSendMessage}>
-          发送
-        </Button>
+      {/* 好友列表区域 */}
+      <div className={styles.friendsSection}>
+        <AccountFriendList
+          accountId={accountId}
+          multiple
+          friendsUpdateState={friendsUpdateState}
+          onChange={(toUserIds: string[]) => {
+            setToUserIds(toUserIds);
+          }}
+        />
       </div>
     </div>
   );
